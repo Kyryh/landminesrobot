f9889ccf2c6af8b2e586c9e434bd9a346ddf79b4
Kyryh <71778401+Kyryh@users.noreply.github.com>
2025-11-02 17:48:47 +0100

    Tweak start command

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import logging
 import os
 
-from telegram import Update
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.constants import ChatType
+from telegram.helpers import create_deep_linked_url
 from telegram.ext import (
     Application,
     CommandHandler,
@@ -23,7 +25,24 @@ logger = logging.getLogger(__name__)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Start")
+    if update.message.chat.type == ChatType.PRIVATE:
+        bot = await context.bot.get_me()
+        await update.message.reply_text(
+            "I'm online! I'll place landmines in any group; try adding me in one!",
+            reply_markup=InlineKeyboardMarkup.from_button(
+                InlineKeyboardButton(
+                    "➕ Add me to a group",
+                    url=create_deep_linked_url(bot.username, "start", group=True),
+                )
+            ),
+        )
+    else:
+        await update.message.reply_text(
+            "I'm online! I'll place landmines in this group; try not to step on one when sending a message!\n"
+            "\n"
+            "Use /place to place some landmines\n"
+            "Use /settings to tweak how rare the landmines are and the punishment for stepping on one"
+        )
 
 
 def main():
