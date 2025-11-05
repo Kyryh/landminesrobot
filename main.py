67503b78155b762793fb8585cb388f1ee26c070d
Kyryh <71778401+Kyryh@users.noreply.github.com>
2025-11-05 22:58:39 +0100

    Add settings command

--- main.py
+++ main.py
@@ -59,6 +59,12 @@ class ChatData:
 ContextType = CallbackContext[ExtBot, dict, ChatData, dict]
 
 
+async def is_admin(update: Update) -> bool:
+    return update.effective_user.id in (
+        admin.user.id for admin in await update.effective_chat.get_administrators()
+    )
+
+
 async def start(update: Update, context: ContextType):
     if update.message.chat.type == ChatType.PRIVATE:
         bot = await context.bot.get_me()
@@ -80,6 +86,96 @@ async def start(update: Update, context: ContextType):
         )
 
 
+def get_settings(user_id: int, context: ContextType):
+    def button(text: str, data=""):
+        return InlineKeyboardButton(text, callback_data=f"{user_id}_{data}")
+
+    punishment_time = (
+        f"{context.chat_data.punishment_time_minutes}m"
+        if context.chat_data.punishment_time_minutes < 60
+        else f"{context.chat_data.punishment_time_minutes / 60}h"
+    )
+    return (
+        "Settings:\n"
+        "\n"
+        f"Current punishment: <b>{context.chat_data.punishment}</b>\n"
+        f"Punishment time: <b>{punishment_time}</b>\n"
+        f"Infinite mines: <b>{context.chat_data.infinite_mines}</b>\n"
+        f"Mines frequence: <b>{context.chat_data.mines_frequence}</b>\n"
+        f"Placed mines: <b>{context.chat_data.placed_mines}</b>",
+        InlineKeyboardMarkup(
+            [
+                [button("⚒️ Punishment")],
+                [
+                    button(
+                        ("✅" if context.chat_data.punishment == "NONE" else "☑️")
+                        + " None",
+                        "punishment_NONE",
+                    ),
+                    button(
+                        ("✅" if context.chat_data.punishment == "BAN" else "☑️")
+                        + " Ban",
+                        "punishment_BAN",
+                    ),
+                    button(
+                        ("✅" if context.chat_data.punishment == "MUTE" else "☑️")
+                        + " Mute",
+                        "punishment_MUTE",
+                    ),
+                ],
+                [button("⏳ Punishment Time")],
+                [
+                    button("➖ Less time", "time_less"),
+                    button("➕ More time", "time_more"),
+                ],
+                [
+                    button(
+                        ("✅" if context.chat_data.infinite_mines else "☑️")
+                        + " Infinite mines",
+                        "infinitemines",
+                    )
+                ],
+                [button("💣 Mines frequence")],
+                [
+                    button(
+                        ("✅" if context.chat_data.mines_frequence == "RARE" else "☑️")
+                        + " Rare",
+                        "frequence_RARE",
+                    ),
+                    button(
+                        (
+                            "✅"
+                            if context.chat_data.mines_frequence == "OCCASIONAL"
+                            else "☑️"
+                        )
+                        + " Occasional",
+                        "frequence_OCCASIONAL",
+                    ),
+                    button(
+                        ("✅" if context.chat_data.mines_frequence == "COMMON" else "☑️")
+                        + " Common",
+                        "frequence_COMMON",
+                    ),
+                ],
+                [button("❌ Remove all mines", "removeall")],
+            ]
+        ),
+    )
+
+
+async def settings(update: Update, context: ContextType):
+    if await is_admin(update):
+        text, reply_markup = get_settings(update.effective_user.id, context)
+        await update.message.reply_html(
+            text,
+            reply_markup=reply_markup,
+        )
+    else:
+        await update.message.reply_text(
+            "Sorry, you're not allowed to use this command."
+        )
+
+
 def main():
     application = (
         Application.builder()
@@ -91,6 +187,7 @@ def main():
     )
 
     application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("settings", settings))
 
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
